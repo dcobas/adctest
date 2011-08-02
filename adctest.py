35df7df553cc3cb568f6abbbb140749088a761c7
Juan David Gonzalez Cobas <dcobas@cern.ch>
2011-08-02 17:59:14 +0200

    add adctest program and filter script

--- adctest.py
+++ adctest.py
@@ -1,19 +1,4 @@
-import wxversion
+import qtGUI as GUI
 
-# We need at least version 2.8 
-wxversion.ensureMinimal('2.8')
-
-# We can start importing modules
-import wx
-import UI.Controller
-
-# Create an application
-app = wx.App(False)
-
-# Create our controller and bind it to the app
-controller = UI.Controller.Controller(app)
-
-# Start the main loop
-app.MainLoop()
-
+GUI.start()
 
--- test/filter.py
+++ test/filter.py
@@ -4,12 +4,12 @@ def adj(x):
     return round(x/5.0 * (2**15))
 
 src = open(argv[1], "r")
-lines = ['\t' + i for i in src.readlines()]
+lines = [int(i.split(", ")[-1][:-1], int(argv[5])) for i in src.readlines()]
 src.close()
 
 dest = open(argv[2], "w")
-dest.write("[SIGNAL]\nnbits = 24\nrate = 1000000\n=ndata = ")
-dest.writelines(lines)
+dest.write("[SIGNAL]\nnbits = %s\nrate = %s\ndata = " % (argv[3], argv[4]))
+dest.writelines('\t%d\n ' % i for i in lines[48:])
 dest.close()    
  
 
