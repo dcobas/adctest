29e32976b8dc4323d36475427428ce91216d785b
Federico Asara <federico@pcbe13300.(none)>
2011-09-01 09:37:53 +0200

    Module initializer loads all the components of the API, if they can be loaded.
    Added support for Pyro4 RemoteObject

--- PAGE/__init__.py
+++ PAGE/__init__.py
@@ -1,13 +1,26 @@
 __author__="Federico"
 __date__ ="$Aug 17, 2011 4:43:08 PM$"
 
-# PAIn: Python ADC Interface
+# PAGE: Python ADC and GEnerators API
 
-import SineWaveform
+hasSis33 = False
+
+import SineWaveform, TTWaveform
 import Agilent33250A
-import Sis33
+import RemoteObject
 
-waveforms = (SineWaveform, )
-generators = (Agilent33250A, )
-adcs = (Sis33, )
+try:
+    import Sis33
+    hasSis33 = True
+except:
+    print 'Error while loading Sis33 module, skipping it'
+
+waveforms = (RemoteObject, SineWaveform, TTWaveform)
+generators = (Agilent33250A, RemoteObject)
+
+if hasSis33:
+    adcs = (RemoteObject, Sis33)
+else:
+    adcs = (RemoteObject, )
+
 
