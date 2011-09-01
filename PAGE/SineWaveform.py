d703f49057b2ebf9f283c6ce2038e99df9c6bf94
Federico Asara <federico@pcbe13300.(none)>
2011-09-01 09:51:36 +0200

    Item API and Pyro4 support added. Added documentation and scale method.

--- PAGE/SineWaveform.py
+++ PAGE/SineWaveform.py
@@ -1,20 +1,23 @@
 import Waveform
 from Utilities import *
 from numpy import *
+import Pyro4
+import Pyro4.util
+import sys
 
 class SineWaveform(Waveform.Waveform):
-    _parameters = {'frequency':('Frequency', 'Frequency of the sinewave, in HZ', 1000, float),
-                  'amplitude':('Amplitude', 'Amplitude of the sinewave, in Vpp', 1, float),
-                  'dc':('DC Compoment', 'DC component of the sinewave, in Vpp', 1, float)}
+    def get(self, what):
+        return self.__getattribute__(what)
+    
+    def set(self, what, how):
+        self.__setattr__(what, how)
+    
+    _parameters = {'frequency':['Frequency', 'Frequency of the sinewave, in HZ', 1000, float],
+                  'amplitude':['Amplitude', 'Amplitude of the sinewave, in Vpp', 1, float],
+                  'dc':['DC Compoment', 'DC component of the sinewave, in Vpp', 0, float]}
                   
     def __init__(self, *args, **kwargs):
-        self.parameters = dict(self._parameters)
-        
-        for i in kwargs: 
-            if i in self.parameters.keys():
-                self.parameters[i][2] = kwargs[i]
-        for i in self.parameters.keys():
-            self.__setattr__(i, self.parameters[i][2])
+        Waveform.Waveform.__init__(self, *args, **kwargs)
     
     def generate(self, sampleRate, samples, nbits, fsr):
         f = self.frequency
@@ -27,3 +30,29 @@ class SineWaveform(Waveform.Waveform):
         lsb = fsr/(2**nbits)
         
         return (s/lsb).astype(int)
+    
+    def scale(self, factor):
+        """Multiply the frequency by factor."""
+        self.frequency *= factor
+        
+        return self
+
+name = 'Sine Waveform'
+target = SineWaveform
+
+import commands
+
+def launch():
+    g = target()
+    hn = commands.getoutput('hostname')
+    
+    daemon=Pyro4.Daemon(host = hn)
+    
+    myUri = daemon.register(g)
+    
+    ns=Pyro4.locateNS()
+    ns.register("Sine", myUri)
+    daemon.requestLoop()
+
+if __name__ == '__main__':
+    launch()
